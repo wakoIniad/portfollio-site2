ec033f50ca64488eaa30940447e7e401577144e2
wakoIniad <s1f102400606@iniad.org>
2026-01-21 08:38:04 +0900

    u

--- background.js
+++ background.js
@@ -23,18 +23,21 @@ const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
 camera.position.set(0, 0, 1000);
 
 // 箱を作成
-const geometry = new THREE.BoxGeometry(50000, 3000, 500);
+//const geometry = new THREE.SphereGeometry(500, 30, 5000);
 const material = new THREE.MeshPhysicalMaterial({
     color: 0x00ffff,
     metalness: 1,
     roughness: 0.1,
     transmission: 1,  // 透過
     thickness: 10,     // 厚みがあると屈折がわかる
-    ior: 2,         // 屈折率
+    ior: 0.8,         // 屈折率
     transparent: true
 });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
+//const geometry2 = new THREE.SphereGeometry(50000, 3000, 500);
+//const box2 = new THREE.Mesh(geometry2, material);
+//scene.add(box2);
 
 // 平行光源
 const light = new THREE.DirectionalLight(0xFFFFFF);
