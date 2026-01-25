b6b7d7f1b16e9ba3d0e6ddb0efae885077c61cce
wakoIniad <s1f102400606@iniad.org>
2026-01-26 02:04:41 +0900

    試しにフッターの色アクセントカラーじゃなくする

--- my-code/chore.js
+++ my-code/chore.js
@@ -11,6 +11,7 @@
 defs.mainBoard.addEventListener('scroll', () => {
     const at = defs.mainBoard.scrollTop / defs.mainBoard.clientHeight;
     //console.log("ki",at);
-    document.querySelector('.board').style.setProperty('--overlay-dark', Math.min(1, at));
+    document.querySelector('body').style.setProperty('--overlay-dark', Math.min(1, at));
+    document.querySelector('body').style.setProperty('--overlay-dark2', at);
 
 })
