e0ba2986c3fdfa47902c39cf909fedae53abca19
wakoIniad <s1f102400606@iniad.org>
2026-01-25 17:22:18 +0900

    submitA

--- my-code/search-works.js
+++ my-code/search-works.js
@@ -3,8 +3,9 @@ document.getElementById("flow-container-scroll-marker").scrollIntoView({
     block: 'center'
 });
 
-import { pipeline } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1/dist/transformers.min.js";
-
+//import { pipeline } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1/dist/transformers.min.js";
+(async()=>{
+const pipeline = MrsXenova.pipeline;
 const pipe = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
 
 //const segmenter = new TinySegmenter();
@@ -102,3 +103,4 @@ function getCosine(a, b) {
         a.reduce((res,v)=>res+v**2,0)**0.5 * b.reduce((res,v)=>res+v**2,0)**0.5
     );
 }
+})();
